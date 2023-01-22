c428fc4e8ffb1b803f2608ca917b8592a18cafd5
Ty <justfacey@gmail.com>
2023-01-22 13:00:12 -0800

    init commit

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def page_routes(page_name):
+  return render_template(page_name)
